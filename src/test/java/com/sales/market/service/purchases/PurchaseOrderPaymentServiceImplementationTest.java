f269a0162d4c104dd93bbe344cbf71ef921acf23
Antony Maceda <antonycc.123@gmail.com>
2021-09-19 19:31:35 -0400

    Se creo un test con junit5 para el funcionamiento de springboottest

--- src/test/java/com/sales/market/service/purchases/PurchaseOrderPaymentServiceImplementationTest.java
+++ src/test/java/com/sales/market/service/purchases/PurchaseOrderPaymentServiceImplementationTest.java
@@ -1,23 +1,36 @@
-package com.sales.market.service;
+package com.sales.market.service.purchases;
 
+import com.sales.market.model.Item;
 import com.sales.market.model.purchases.*;
-import com.sales.market.service.purchases.PurchaseOrderPaymentServiceImpl;
+import com.sales.market.service.ItemServiceImpl;
+import com.sales.market.service.PurchaseOrderServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-public class PurchaseOrderPaymentServiceImplTest {
+class PurchaseOrderPaymentServiceImplementationTest {
 
     @Autowired
     PurchaseOrderPaymentServiceImpl purchaseOrderPaymentService;
 
     @Autowired
     PurchaseOrderServiceImpl purchaseOrderService;
+
+    @Autowired
+    PurchaseOrderDetailServiceImpl purchaseOrderDetailService;
+
+    @Autowired
+    MeasureUnitServiceImpl measureUnitService;
+
+    @Autowired
+    ItemServiceImpl itemService;
 
     @Test
     public void testRegisterOrderPaymentWhenPurchaseOrderPaymentKindIsPartial () {
@@ -29,10 +42,9 @@ public class PurchaseOrderPaymentServiceImplTest {
                 purchaseOrder);
         purchaseOrderPaymentService.registerOrderPayment(null);
         PurchaseOrder purchaseOrderUpdated = purchaseOrderService.findById(purchaseOrderPayment.getPurchaseOrder().getId());
-
-        Assert.assertEquals(purchaseOrderUpdated.getBalanceAmount(), new BigDecimal("400"));
-        Assert.assertEquals(purchaseOrderUpdated.getPaymentStatus(), PurchaseOrderPaymentStatus.PARTIAL_PAYMENT);
-        Assert.assertEquals(purchaseOrderUpdated.getState(), PurchaseOrderState.FIN);
+        assertEquals(purchaseOrderUpdated.getBalanceAmount(), new BigDecimal("400"));
+        assertEquals(purchaseOrderUpdated.getPaymentStatus(), PurchaseOrderPaymentStatus.PARTIAL_PAYMENT);
+        assertEquals(purchaseOrderUpdated.getState(), PurchaseOrderState.FIN);
     }
 
     @Test
@@ -46,9 +58,9 @@ public class PurchaseOrderPaymentServiceImplTest {
         purchaseOrderPaymentService.registerOrderPayment(null);
         PurchaseOrder purchaseOrderUpdated = purchaseOrderService.findById(purchaseOrderPayment.getPurchaseOrder().getId());
 
-        Assert.assertEquals(purchaseOrderUpdated.getBalanceAmount(), BigDecimal.ZERO);
-        Assert.assertEquals(purchaseOrderUpdated.getPaymentStatus(), PurchaseOrderPaymentStatus.FULLY_PAID);
-        Assert.assertEquals(purchaseOrderUpdated.getState(), PurchaseOrderState.LIQ);
+        assertEquals(purchaseOrderUpdated.getBalanceAmount(), BigDecimal.ZERO);
+        assertEquals(purchaseOrderUpdated.getPaymentStatus(), PurchaseOrderPaymentStatus.FULLY_PAID);
+        assertEquals(purchaseOrderUpdated.getState(), PurchaseOrderState.LIQ);
     }
 
     public void testRegisterOrderPaymentWhenPurchaseOrderPaymentKindIsTotalAndPayAmountIsNotEnoughShouldFail () {
@@ -56,6 +68,10 @@ public class PurchaseOrderPaymentServiceImplTest {
     }
 
     public PurchaseOrder createPurchaseOrder (String orderNumber) {
+        Provider provider = new Provider();
+        provider.setCode("TEST_PROVIDER-CODE");
+        provider.setName("TEST PROVIDER NAME");
+
         PurchaseOrder purchaseOrder = new PurchaseOrder();
         purchaseOrder.setOrderNumber(orderNumber);
         purchaseOrder.setDate(new Date());
@@ -63,6 +79,30 @@ public class PurchaseOrderPaymentServiceImplTest {
         purchaseOrder.setPaymentStatus(PurchaseOrderPaymentStatus.NO_PAYMENT);
         purchaseOrder.setTotalAmount(new BigDecimal("500"));
         purchaseOrder.setBalanceAmount(new BigDecimal("500"));
+        purchaseOrder.setProvider(provider);
+
+        PurchaseOrderDetail detail = new PurchaseOrderDetail();
+
+        MeasureUnit measureUnit = new MeasureUnit();
+        measureUnit.setDescription("TEST unit description");
+        measureUnit.setName("TEST UNIT");
+        measureUnit.setMeasureUnitCode("U");
+        measureUnitService.save(measureUnit);
+
+        Item item = new Item();
+        item.setCode("it");
+        item.setName("item name");
+        itemService.save(item);
+
+//        detail.setPurchaseOrder(purchaseOrder);
+        detail.setQuantity(new BigDecimal("10"));
+        detail.setMeasureUnit(measureUnit);
+        detail.setItem(item);
+        detail.setItemCode("TEST-item-code");
+        detail.setProviderItemCode("TEST-PROVIDER-item-code");
+        purchaseOrderDetailService.save(detail);
+        purchaseOrder.setPurchaseOrderDetailList(Arrays.asList(detail));
+        purchaseOrder.setDefaultDetail(detail);
         return purchaseOrderService.save(purchaseOrder);
     }
 
